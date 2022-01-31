21cfb00b7ca7a19a70bd893f9f6d1a88fa31a216
stansful <stansful@gmail.com>
2022-01-31 15:13:09 +0300

    feature: user, profile entities

---
+++ src/user/profile.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './user.entity';
+
+@Entity()
+export class Profile {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  profileName: string;
+
+  @Column({ enum: ['male', 'female', 'alien'] })
+  gender: string;
+
+  @Column()
+  aboutMe: string;
+
+  @OneToOne(() => User, (user) => user.profile, {
+    cascade: true,
+  })
+  user: User;
+}
---
+++ src/user/user.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Profile } from './profile.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @OneToOne(() => Profile)
+  @JoinColumn()
+  profile: Profile;
+}
