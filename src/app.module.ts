2b37b4cef78b740f6ceb145321f03fbcd99cca2c
stansful <stansful@gmail.com>
2022-01-31 15:16:57 +0300

    feature: app module update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigurationModule } from './config/configuration.module';
+import { UserModule } from './user/user.module';
+import { HashModule } from './hash/hash.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmConfigService } from './config/typeOrmConfig.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigurationModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+    UserModule,
+    HashModule,
+  ],
 })
 export class AppModule {}
