072639d1cb6ff4929b07db688a2a61c2e0b84061
stansful <stansful@gmail.com>
2022-01-31 15:15:16 +0300

    build: global port configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ApiConfiguration, ConfigTypes } from './config/apiConfiguration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService: ConfigService<ApiConfiguration> = app.get(ConfigService);
+  const port = configService.get(ConfigTypes.PORT);
+
+  await app.listen(port, () =>
+    console.log(`Server on http://localhost:${port}`),
+  );
 }
+
 bootstrap();
