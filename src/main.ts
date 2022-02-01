51bfc42a6e40f3eeb6629eeb93ac443bbe5538fa
stansful <stansful@gmail.com>
2022-02-01 12:10:50 +0300

    feature: validation pipe & cookie parser

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ApiConfiguration, ConfigTypes } from './config/apiConfiguration';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const configService: ConfigService<ApiConfiguration> = app.get(ConfigService);
   const port = configService.get(ConfigTypes.PORT);
