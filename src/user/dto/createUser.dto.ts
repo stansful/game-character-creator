48cc5411242e0c57d3fb634dc86cc4e47e161bb5
stansful <stansful@gmail.com>
2022-02-01 12:11:07 +0300

    feature: createUser validation

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -1,5 +1,21 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
   username: string;
+
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
   password: string;
 }
