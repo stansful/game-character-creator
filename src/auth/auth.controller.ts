504f6f690998b5240009933868295fbd50cb89a9
stansful <stansful@gmail.com>
2022-02-03 15:53:36 +0300

    feature: remove get endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,30 +1,19 @@
 import {
   Body,
   Controller,
-  Get,
   HttpCode,
   HttpStatus,
   Post,
-  Req,
   Res,
-  UseGuards,
 } from '@nestjs/common';
 import { SignUpDto } from './dto/signUp.dto';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signIn.dto';
 import { Response } from 'express';
-import { JwtGuard } from './guards/jwt.guard';
-import { ReqUserInterface } from './interfaces/reqUser.interface';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @UseGuards(JwtGuard)
-  @Get()
-  async giveMeMyJwtInfo(@Req() req: ReqUserInterface) {
-    return req.user;
-  }
 
   @Post('signUp')
   async registration(@Body() signUp: SignUpDto) {
