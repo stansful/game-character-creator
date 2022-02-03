6028d144fde831da326330d19dee39a094c86ad3
stansful <stansful@gmail.com>
2022-02-03 15:54:18 +0300

    feature: serialization

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Param,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get(':id')
   async getUser(@Param('id') id: number) {
     return this.userService.getById(id);
